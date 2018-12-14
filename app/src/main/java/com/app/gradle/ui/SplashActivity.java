198e992351ae2e5615d999e2524a7a1294e08be5
umeshbsa <umesh.bsa@gmail.com>
2018-12-14 13:17:31 +0530

    Added code in Splash screen from app.gradle task, By using jenkins script.

--- app/src/main/java/com/app/gradle/ui/SplashActivity.java
+++ app/src/main/java/com/app/gradle/ui/SplashActivity.java
@@ -1,15 +1,31 @@
 package com.app.gradle.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.app.gradle.BuildConfig;
 import com.app.gradle.R;
 
-public abstract class SplashActivity extends BaseActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        View bgHeader = findViewById(R.id.bg_header);
+        ImageView splashLogo = (ImageView) findViewById(R.id.splash_logo);
+
+        // This is coming from gralde file. This color comes from Jenkins script when we run script command.
+        int bgColor = Color.parseColor(BuildConfig.PRIMARY_COLOR);
+        bgHeader.setBackgroundColor(bgColor);
+
+        // This is coming from gralde file. This image comes from Jenkins script when we run script command.
+        splashLogo.setImageResource(R.drawable.splash_logo);
+
     }
 }
